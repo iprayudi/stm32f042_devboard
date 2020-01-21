195dcae9056935c4cc43c607e03fd635ddb6ad30
Iman Prayudi <imprayudi@PI.AC.AE>
2020-01-21 08:15:36 +0400

    added example for fft transform (using Adafruit_ZeroFFT library)

---
+++ codes/fft-example/stm32f042_dev/Debug/Drivers/ADAFRUIT_ZEROFFT/example_data.d
@@ -0,0 +1,8 @@
+Drivers/ADAFRUIT_ZEROFFT/example_data.o: \
+ ../Drivers/ADAFRUIT_ZEROFFT/example_data.c \
+ ../Drivers/ADAFRUIT_ZEROFFT/Adafruit_ZeroFFT.h \
+ ../Drivers/ADAFRUIT_ZEROFFT/arm_common_tables.h
+
+../Drivers/ADAFRUIT_ZEROFFT/Adafruit_ZeroFFT.h:
+
+../Drivers/ADAFRUIT_ZEROFFT/arm_common_tables.h:
---
+++ codes/fft-example/stm32f042_dev/Drivers/SSD1306/ssd1306_tests.h
@@ -0,0 +1,12 @@
+/*
+ * https://github.com/afiskon/stm32-ssd1306
+ */
+#ifndef __SSD1306_TEST_H__
+#define __SSD1306_TEST_H__
+
+void ssd1306_TestBorder();
+void ssd1306_TestFonts();
+void ssd1306_TestFPS();
+void ssd1306_TestAll();
+
+#endif // __SSD1306_TEST_H__
